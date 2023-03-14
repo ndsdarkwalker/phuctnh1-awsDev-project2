659225e624a4c9ea6b680853e6411f909abc2822
Phuc Hoang <ndsdarkwalker@gmail.com>
2023-03-14 22:48:57 +0700

    aws cloud ElasticBean

---
+++ src/util/util.ts
@@ -0,0 +1,42 @@
+const axios = require('axios').default;
+import fs from "fs";
+import Jimp = require("jimp");
+
+// filterImageFromURL
+// helper function to download, filter, and save the filtered image locally
+// returns the absolute path to the local image
+// INPUTS
+//    inputURL: string - a publicly accessible url to an image file
+// RETURNS
+//    an absolute path to a filtered image locally saved file
+export async function filterImageFromURL(inputURL: any): Promise<string> {
+  try {
+    const { data: imageBuffer } = await axios({
+      method: 'get',
+      url: inputURL,
+      responseType: 'arraybuffer'
+    });
+    const photo = await Jimp.read(imageBuffer);
+    const outpath = `/tmp/filtered.${Math.floor(Math.random() * 2000)}.jpg`;
+    const path = __dirname + outpath;
+    const photoResult = await photo
+      .resize(256, 256) // resize
+      .quality(60) // set JPEG quality
+      .greyscale() // set greyscale
+      .writeAsync(path);
+    return path;
+  } catch (error) {
+    // console.log('ERROR ===============> ', error);
+  }
+}
+
+// deleteLocalFiles
+// helper function to delete files on the local disk
+// useful to cleanup after tasks
+// INPUTS
+//    files: Array<string> an array of absolute paths to files
+export async function deleteLocalFiles(files: Array<string>) {
+  for (let file of files) {
+    fs.unlinkSync(file);
+  }
+}
